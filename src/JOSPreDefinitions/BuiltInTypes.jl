f6317bbbce98b5407f4ecd0fb811735bf55a3dbc
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-09-05 18:27:18 +0100

    #23 - Add builtin types

--- src/JOSPreDefinitions/BuiltInTypes.jl
+++ src/JOSPreDefinitions/BuiltInTypes.jl
@@ -2,7 +2,24 @@ export _Exception, _TypeException, _Vector, _Pair, _Symbol
 
 @defbuiltin _Exception(Exception)
 @defbuiltin _TypeException(Type{Exception})
-@defbuiltin _Vector(Vector)
-@defbuiltin _Pair(Pair)
 @defbuiltin _Symbol(Symbol)
 @defbuiltin _IO(IO)
+
+@defbuiltin _Int8(Int8)
+@defbuiltin _Int16(Int16)
+@defbuiltin _Int32(Int32)
+@defbuiltin _Int64(Int64)
+@defbuiltin _Int128(Int128)
+
+@defbuiltin _Bool(Bool)
+
+@defbuiltin _Char(Char)
+@defbuiltin _String(String)
+
+@defbuiltin _Float16(Float16)
+@defbuiltin _Float32(Float32)
+@defbuiltin _Float64(Float64)
+
+@defbuiltin _Vector(Vector)
+@defbuiltin _Pair(Pair)
+@defbuiltin _NamedTuple(NamedTuple)
