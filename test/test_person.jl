ba0fc4c738c1146b0104f7de4fe893185823e381
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-09-08 09:07:31 +0100

    #16 - Improve Person test

--- test/test_person.jl
+++ test/test_person.jl
@@ -2,19 +2,23 @@ using JuliaObjectSystem
 using Test
 
 @testset "Person" begin
-    @defclass(SpecialMetaclass, [Class], [])
+    @defclass(UndoableClass, [Class], [])
 
-    @test class_of(SpecialMetaclass) === Class
+    @test class_of(UndoableClass) === Class
 
     @defclass(Person, [],
-    [name="John",
-    [age, initform=40]],
-    metaclass = SpecialMetaclass)
+    [
+        name="John",
+        [age, initform=40],
+        friend
+    ],
+    metaclass = UndoableClass)
 
-    @test class_of(Person) === SpecialMetaclass
+    @test class_of(Person) === UndoableClass
     @test Person.direct_superclasses == [Object]
 
     p1 = new(Person)
     @test p1.name == "John"
     @test p1.age == 40
+    @test ismissing(p1.friend)
 end
