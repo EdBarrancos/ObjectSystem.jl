6f309e8e05eb945f8e2f00f0b00b2e43b02af50b
Eduardo Barrancos <eduardobarrancos11488@gmail.com>
2023-09-11 19:31:41 +0100

    #37 - Update instantiation protocol tests

--- test/test_instantiation_protocol.jl
+++ test/test_instantiation_protocol.jl
@@ -9,3 +9,19 @@ using Test
     @test length(getfield(simple_instance, :slots)) == 0
 end
 
+@defclass(CountingClass, [Class], [counter=0])
+
+@defmethod allocate_instance(class::CountingClass) = begin
+    class.counter += 1
+    call_next_method()
+end
+
+@defclass(Foo, [], [], metaclass=CountingClass)
+@defclass(Bar, [], [], metaclass=CountingClass)
+
+new(Foo)
+new(Foo)
+new(Bar)
+
+@test Foo.counter == 2
+@test Bar.counter == 1
